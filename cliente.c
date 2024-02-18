9b145fd8e34025b695f6c9f5ea49540af44bea26
AntonioErick <groot150x@gmail.com>
2024-02-18 17:00:31 -0300

    ler opçao e limpabuffer

--- cliente.c
+++ cliente.c
@@ -1,7 +1,31 @@
 #include "cliente.h"
 
-void ler_arquivo(FILE *arquivo_client, clientes **usuarios, int *total_de_Usuarios) 
-{
+void limpabuffer(void){
+    char buffer[100];
+    fgets(buffer, sizeof(buffer), stdin); // Lê uma linha inteira de entrada
+}
+
+void ler_opcao(int menor_valor, int maior_valor){
+    int escolha;
+    char entrada[50];
+
+     do {
+        printf("Escolha uma opção: ");
+        scanf(" %[^\n]", entrada);
+
+        escolha = entrada[0];
+
+        if (escolha >= menor_valor && escolha <= maior_valor) {
+            limpabuffer();
+            break;
+        } else {
+            printf("Opcao invalida. A opcao deve estar entre %d e %d. \n", menor_valor, maior_valor);
+            limpabuffer();
+        }
+    } while (escolha != 4);
+}
+
+void ler_arquivo(FILE *arquivo_client, clientes **usuarios, int *total_de_Usuarios) {
     while (fscanf(arquivo_client, "Nome: %19[^\n]\nEndereço: %19[^\n]\nCódigo de Cliente: %d\n", (*usuarios)[*total_de_Usuarios].nome, (*usuarios)[*total_de_Usuarios].endereco, &(*usuarios)[*total_de_Usuarios].codigo_de_cliente) != EOF) {
         (*total_de_Usuarios)++;
         *usuarios = (clientes *)realloc(*usuarios, (*total_de_Usuarios + 1) * sizeof(clientes));
@@ -17,7 +41,7 @@ void menu()
     printf("\nMenu de Opções:\n"
            "1. Cadastrar Usuarios\n"
            "2. Alterar Dados\n"
-           "3. Ordenação dos Códigos\n"
+           "3. Opção 3\n"
            "4. Sair\n");
 }
 
@@ -57,14 +81,14 @@ void alterar_dados(clientes *usuarios, int total_de_Usuarios, FILE *arquivo_clie
     {
         printf("Digite o codigo do cliente que deseja alterar: ");
         scanf("%d", &codigo);
-        fclose(arquivo_client);
-        arquivo_client = fopen("arquivo_dados_dos_clientes.txt", "w");
-        bool valido = false;
+        if (codigo >= 0 && codigo <= 999)
+        {
+            fclose(arquivo_client);
+            arquivo_client = fopen("arquivo_dados_dos_clientes.txt", "w");
             for (int i = 0; i < total_de_Usuarios; i++)
             {
                 if (usuarios[i].codigo_de_cliente == codigo)
                 {
-                    valido = true;
                     printf("Digite o novo nome do cliente: ");
                     scanf(" %[^\n]", usuarios[i].nome);
                     printf("Digite o novo endereço do cliente: ");
@@ -86,13 +110,14 @@ void alterar_dados(clientes *usuarios, int total_de_Usuarios, FILE *arquivo_clie
                     fprintf(arquivo_client, "\n");
                 }
             }
-
-    if (valido == false)
-    {
-        printf("---------------------------\n");
-        printf("Codigo de cliente não encontrado!\n");
-        printf("---------------------------\n");
-    }
+        }
+        else
+        {
+            printf("---------------------------\n");
+            printf("Codigo de cliente não encontrado!\n");
+            printf("Digite novamente o codigo do cliente\n ");
+            printf("---------------------------\n");
+        }
     } while (codigo < 0 || codigo > 999);
 }
 
@@ -105,70 +130,78 @@ void imprimir_dados(clientes *usuarios, int total_de_Usuarios)
     }
 }
 
-void merge(clientes * usuarios, int esquerda, int meio, int direita) 
+void merge(clientes * usuarios, int l, int m, int r) 
 { 
     int i, j, k; 
-    int n1 = meio - esquerda + 1; 
-    int n2 = direita - meio; 
+    int n1 = m - l + 1; 
+    int n2 = r - m; 
   
-    clientes Esquerda[n1], Direita[n2]; 
+    clientes L[n1], R[n2]; 
   
     for (i = 0; i < n1; i++) 
-        Esquerda[i] = usuarios[esquerda + i]; 
+        L[i] = usuarios[l + i]; 
     for (j = 0; j < n2; j++) 
-        Direita[j] = usuarios[meio + 1 + j]; 
+        R[j] = usuarios[m + 1 + j]; 
   
     i = 0; 
     j = 0; 
-    k = esquerda;
-     
-    while (i < n1 && j < n2) 
-    { 
-        if (Esquerda[i].codigo_de_cliente <= Direita[j].codigo_de_cliente) { 
-            usuarios[k] = Esquerda[i]; 
+  
+    k = l; 
+    while (i < n1 && j < n2) { 
+        if (L[i].codigo_de_cliente <= R[j].codigo_de_cliente) { 
+            usuarios[k] = L[i]; 
             i++; 
         } 
         else { 
-            usuarios[k] = Direita[j]; 
+            usuarios[k] = R[j]; 
             j++; 
         } 
         k++; 
     } 
   
-    while (i < n1) 
-    { 
-        usuarios[k] = Esquerda[i]; 
+    while (i < n1) { 
+        usuarios[k] = L[i]; 
         i++; 
         k++; 
     } 
 
-    while (j < n2) 
-    { 
-        usuarios[k] = Direita[j]; 
+    while (j < n2) { 
+        usuarios[k] = R[j]; 
         j++; 
         k++; 
-    }
-} 
-  
-void mergeSort(clientes * usuarios, int esquerda, int direita) 
-{ 
-    if (esquerda < direita) { 
-        int meio = esquerda + (direita - esquerda) / 2; 
-  
-        mergeSort(usuarios, esquerda, meio); 
-        mergeSort(usuarios, meio + 1, direita); 
-  
-        merge(usuarios, esquerda, meio, direita); 
     } 
 } 
   
+void mergeSort(clientes * usuarios, int l, int r) 
+{ 
+    if (l < r) { 
+        int m = l + (r - l) / 2; 
+  
+        mergeSort(usuarios, l, m); 
+        mergeSort(usuarios, m + 1, r); 
+  
+        merge(usuarios, l, m, r); 
+    } 
+} 
+  
+void printArray(clientes *usuarios, int size) 
+{ 
+    int i; 
+    for (i = 0; i < size; i++) 
+        printf("%d ", usuarios[i].codigo_de_cliente); 
+    printf("\n"); 
+}
+
 void alterar_dados_merge(clientes *usuarios, int total_de_Usuarios, FILE *arquivo_client)
 {
     arquivo_client = fopen("arquivo_dados_dos_clientes.txt", "w");
-        for (int i = 0; i < total_de_Usuarios; i++) {        
-        fprintf(arquivo_client, "Nome: %s\n", usuarios[i].nome);
-        fprintf(arquivo_client, "Endereço: %s\n", usuarios[i].endereco);
-        fprintf(arquivo_client, "Código de Cliente: %d\n", usuarios[i].codigo_de_cliente);
-        fprintf(arquivo_client, "\n");
+            for (int i = 0; i < total_de_Usuarios; i++)
+            {
+                {
+                    fprintf(arquivo_client, "Nome: %s\n", usuarios[i].nome);
+                    fprintf(arquivo_client, "Endereço: %s\n", usuarios[i].endereco);
+                    fprintf(arquivo_client, "Código de Cliente: %d\n", usuarios[i].codigo_de_cliente);
+                    fprintf(arquivo_client, "\n");
+                }
+            }
         }
-}
